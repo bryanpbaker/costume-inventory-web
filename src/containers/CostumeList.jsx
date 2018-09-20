026c658a60812510aa372b0d5d32f261dc35df22
Bryan Baker <bryanpbaker@gmail.com>
2018-09-20 13:44:30 -0500

    catching up

--- src/containers/CostumeList.jsx
+++ src/containers/CostumeList.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class CostumeList extends Component {
+  async componentDidMount() {
+    const response = await fetch('http://localhost:5000/api/v1/costumes');
+    const costumes = await response.json();
+
+    console.log(costumes);
+  }
+
   render() {
     return (
       <div>
